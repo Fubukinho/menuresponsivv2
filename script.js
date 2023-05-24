8b67c6e6147ceb2254691552741bf3dab3923316
Fubukinho <134311984+Fubukinho@users.noreply.github.com>
2023-05-24 10:32:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function abrirMenu() {
+    let open = document.getElementById('menu-mobile-fundo').style.width = '50%';
+}
+
+function fecharMenu() {
+    let close = document.getElementById('menu-mobile-fundo').style.width = '0%';
+}
